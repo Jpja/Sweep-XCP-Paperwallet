2947ac4df6b5c5d142426ae98553e32810d91826
Jpja <jpjanss@gmail.com>
2017-06-10 08:15:37 +0300

    Create step2privkey.js

--- project/step2privkey.js
+++ project/step2privkey.js
@@ -14,7 +14,7 @@ function validateInputPrivKey() {
     var output = "";
     if (isValidPrivKey(privKeyInput)) {
 		var addr = privKeyToAddress(privKeyInput);
-        output = addr + "<i><br>- View on <a href=\"https://counterpartychain.io/address/"+addr+"\"  target=\"_blank\">CounterpartyChain</a><br>- View on <a href=\"http://blockscan.com/address/"+addr+"\"  target=\"_blank\">Blockscan</a><br>- View on <a href=\"https://www.blocktrail.com/BTC/address/"+addr+"\"  target=\"_blank\">Blocktrail</a><br>- View on <a href=\"https://blockchain.info/address/"+addr+"\"  target=\"_blank\">Blockchain.info</a></i>";
+        output = addr + "<i><br>- View on <a href=\"https://xchain.io/address/"+addr+"\"  target=\"_blank\">XChain</a><br>- View on <a href=\"https://www.blocktrail.com/BTC/address/"+addr+"\"  target=\"_blank\">Blocktrail</a><br>- View on <a href=\"https://blockchain.info/address/"+addr+"\"  target=\"_blank\">Blockchain.info</a></i>";
     } else {
 	 output = "<i>Address appears here when correct private key is entered</i>";
 	}
